d200a6df26ebdf7c9fb0ea38eeec40ca306c2678
QY-Ver0 <lqypublic@gmail.com>
2025-03-27 00:02:34 +0800

    Updated event test (time constraint, can't rlly test more unless I provide template database)

--- test/eventController.test.js
+++ test/eventController.test.js
@@ -30,3 +30,39 @@ describe('GET /api/events/org', () => {
         .end(done);
     });
 });
+
+// describe('GET DEFAULT /api/events/findBy', () => {
+//     it('should return a list of events', (done) => {
+//         request(app)
+//         .get('/api/events/findBy')
+//         .expect(200)
+//         .expect((res) => {
+//             if (!Array.isArray(res.body.data)) throw new Error('Response is not an array, expected one.');
+//         })
+//         .end(done);
+//     });
+// });
+// const event1 = {
+//     eventID: 1,
+//     userID: null,
+//     eventOrgID: null,
+//     startDateTime: new Date(),
+//     title: 'An Upcoming Event',
+//     isVisible: true,
+// };
+
+// describe('GET /api/events/1 ', () => {
+//     it('Retrieves event with id 1', (done) => {
+//         request(app).get('/api/events/1')
+//         .expect(200)
+//         .expect((res) => {
+//             // throw new Error(res.body.data.title);
+//             const { data } = res.body;
+//             if (!res.body) throw new Error(`Event with this ID don't exist.`);
+//             if (data.eventID != 1) throw new Error(`EventID retrieved is ${data.eventID}, expected 1.`);
+//         })
+//         .end(done);
+//         // expect(status).to.equal(200);
+//         // expect(data).to.deep.include(event1);
+//     });
+// });
